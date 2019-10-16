195f32e996ac688523e72afe8c7dcbab984225ec
s344951241 <344951241@qq.com>
2019-10-16 21:43:35 +0800

    mysql for python

--- python/pt/hello.py
+++ python/pt/hello.py
@@ -25,8 +25,54 @@ mycursor = mydb.cursor()
 #mycursor.execute("ALTER TABLE sites ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
 #mycursor.execute("CREATE TABLE sites (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), url VARCHAR(255))")
 
+#插入数据
 # sql = "INSERT INTO sites(name,url) VALUES (%s,%s)"
 # val = ("RUNOOB","https://www.runoob.com")
 # mycursor.execute(sql,val)
 # mydb.commit()
 # print(mycursor.rowcount,"记录插入成功")
+
+#批量插入
+# sql = "INSERT INTO sites(name,url) VALUES(%s,%s)"
+# val =[
+#   ('Google', 'https://www.google.com'),
+#   ('Github', 'https://www.github.com'),
+#   ('Taobao', 'https://www.taobao.com'),
+#   ('stackoverflow', 'https://www.stackoverflow.com/')
+# ]
+# mycursor.executemany(sql,val)
+# mydb.commit()
+# print(mycursor.rowcount,"记录插入成功")
+
+#print(mycursor.lastrowid)
+
+#查询
+# sql = "select * from sites"
+# sql = "select * from sites where name = 'RUNOOB'"
+# sql = "select * from sites where url like '%oo%'"
+# mycursor.execute(sql)
+# myresult = mycursor.fetchall() #fetchall获取所有记录  fetchone #第一条记录
+# for x in myresult:
+#   print(x)
+
+# sql = "select * from sites where name = %s"
+# val = ("RUNOOB",)
+# mycursor.execute(sql,val)
+# myresult = mycursor.fetchall()
+# for x in myresult:
+#   print(x)
+
+#删
+# sql = "delete from sites where name='stackoverflow'"
+# mycursor.execute(sql)
+# mydb.commit()
+# print(mycursor.rowcount," 条记录删除")
+
+#改
+# sql = "update sites set name='tb' where name='Taobao'"
+# mycursor.execute(sql)
+# mydb.commit()
+# print(mycursor.rowcount,'条记录修改')
+
+# sql = "drop table if exists sites"
+# mycursor.execute(sql)
